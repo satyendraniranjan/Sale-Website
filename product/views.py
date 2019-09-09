b8d2e0fc1352001dc77305179decd7a7f91e50d6
sat.n760@gmail.com <sat.n760@gmail.com>
2019-09-10 01:00:41 +0530

    Added productlist and productdetail view.

--- product/views.py
+++ product/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import Product , ProductImages , Category
+from django.core.paginator import Paginator
+from django.db.models import Count
+from django.db.models import Q
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+def productlist(request , category_slug=None):
+    category = None
+    productlist = Product.objects.all()
+
+    template = 'Product/product_list.html'
+    context = {'product_list': productlist}
+    return render(request, template, context)
+
+
+def productdetail(request, id):
+    productdetail= Product.objects.all()
+
+    template = 'Product/product_detail.html'
+    context = {'product_detail': productdetail}
+    return render(request, template, context)
+
+
