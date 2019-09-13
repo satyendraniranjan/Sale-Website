ce06e002495f87cff0929004881f2959d0cacf88
sat.n760@gmail.com <sat.n760@gmail.com>
2019-09-13 23:28:33 +0530

    Added All Ads in templates.

--- product/urls.py
+++ product/urls.py
@@ -7,6 +7,6 @@ app_name = 'product'
 urlpatterns = [
     path('' , views.productlist , name='product_list') ,
 #    path('<int:id>' , views.productdetail , name='product_detail') ,
-#    path('<slug:category_slug>' , views.productlist , name='product_list_category') ,
+    path('<slug:category_slug>' , views.productlist , name='product_list_category') ,
     path('<slug:product_slug>' , views.productdetail , name='product_detail')
 ]
--- product/views.py
+++ product/views.py
@@ -11,15 +11,17 @@ def productlist(request , category_slug=None):
     productlist = Product.objects.all()
     categorylist = Category.objects.annotate(total_products=Count('product'))
 
-    template = 'Product/product_list.html'
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        productlist = productlist.filter(category=category)
+
+
 
     paginator = Paginator(productlist, 1)  # Show 25 contacts per page
-
     page = request.GET.get('page')
     productlist = paginator.get_page(page)
-
-
-    context = {'product_list': productlist, 'category_list': categorylist}
+    template = 'Product/product_list.html'
+    context = {'product_list': productlist, 'category_list': categorylist, 'category': category }
     return render(request, template, context)
 
 
