11764307cc4ee2befeca87d0ad2a52f2c28a5333
sat.n760@gmail.com <sat.n760@gmail.com>
2019-09-16 19:39:24 +0530

    Fixing views and url.

--- product/urls.py
+++ product/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('' , views.productlist , name='product_list') ,
 #    path('<int:id>' , views.productdetail , name='product_detail') ,
     path('<slug:category_slug>' , views.productlist , name='product_list_category') ,
-    path('<slug:product_slug>' , views.productdetail , name='product_detail')
+    path('detail/<slug:product_slug>' , views.productdetail , name='product_detail')
 ]
