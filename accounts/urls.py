f018826743e45ac847c954fc9f4c1f39d341667c
sat.n760@gmail.com <sat.n760@gmail.com>
2019-09-17 23:38:21 +0530

    Added templates for password reset and password change.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import login
+from django.contrib.auth import views as auth_views
 
 
 app_name = 'accounts'
 
 urlpatterns = [
 #    path('login/' , login ,{'template_name': 'accounts/login.html'}, name='name'),
+    path('login/', auth_views.LoginViews.as_view(), name='login'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+
+
+
 ]
